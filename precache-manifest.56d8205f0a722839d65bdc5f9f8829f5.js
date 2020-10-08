3576971ad75bf42d5549d90d56eef14491d487d7
Arturo Bravo <aarturoo97@gmail.com>
2020-10-08 17:37:27 -0300

    Updates

---
+++ precache-manifest.56d8205f0a722839d65bdc5f9f8829f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118c7e08836ef0f71928310f4890875a",
+    "url": "/parcialI/index.html"
+  },
+  {
+    "revision": "7fe824da0e93bfa699a9",
+    "url": "/parcialI/static/css/main.c9d4df3f.chunk.css"
+  },
+  {
+    "revision": "e7e9bedf6b880db25320",
+    "url": "/parcialI/static/js/2.35f38058.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/parcialI/static/js/2.35f38058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe824da0e93bfa699a9",
+    "url": "/parcialI/static/js/main.8cfd14eb.chunk.js"
+  },
+  {
+    "revision": "a81506759db76824a7c8",
+    "url": "/parcialI/static/js/runtime-main.cf1810ab.js"
+  },
+  {
+    "revision": "7a7aea73017bd48a840a5c627906b51a",
+    "url": "/parcialI/static/media/logouai.7a7aea73.png"
+  }
+]);
